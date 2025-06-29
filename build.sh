7427059d753ac9cf707aad4e0d3838f94fb85ed0
Jay <Hawaiian647@gmail.com>
2025-06-28 20:44:06 -0400

    made some change to bash script

--- build.sh
+++ build.sh
@@ -7,15 +7,16 @@
 # source ../../intel/oneapi/setvars.sh # FOR SDSU FERMI ONLY
 # source $HOME/Code/intel/oneapi/setvars.sh # FOR LOCAL LINUX ONLY
  
-# USED A CONDA ENV WITH CANTERA INSTALLED IN ENV AND OTHER SELF ADDED PATHS
-#icpx -O3 -L../cantera/build/lib -I../cantera/include -c reactor.cpp -lcantera
+#compile cpp with linking cantera
+# icpx -O3 -L../cantera/build/lib -I../cantera/include -c reactor.cpp -lcantera
 icpx -O3 -L$CONDA_PREFIX/lib -I$CONDA_PREFIX/include -c reactor.cpp -lcantera
-#ifx -O3 -L../cantera/build/lib -I../cantera/include -o main.exe driver_samples.f90 reactor.o \
+
+# compile f90 linking cantera and cpp 
+# ifx -O3 -L../cantera/build/lib -I../cantera/include -o main.exe driver_samples.f90 reactor.o \
 #	-lstdc++ -lcantera -Bstatic -L../ISAT/lib -I../ISAT/isatab_ser -lisat7_ser -L -mkl -Bdynamic -mkl -lstdc++
 ifx -O3 -L$CONDA_PREFIX/lib -I$CONDA_PREFIX/include -o main.exe driver_samples.f90 reactor.o \
 	-lstdc++ -lcantera -Bstatic -L../isat/ISAT/lib -I../isat/ISAT/isatab_ser -lisat7_ser -qmkl \
 	-Bdynamic -qmkl -Wl,-rpath,$CONDA_PREFIX/lib
 
-# SOURCE VARIABLES AGIAN AND RUN EXE
-# source ../../intel/oneapi/setvars.sh
-./main.exe
+#run program
+# ./main.exe
