a73ca06ad976117524ffe00b4f1259d0ac0c7314
Jay <hawaiian647@gmail.com>
2025-09-13 11:01:07 -0700

    stuff.

--- preconditioning_for_linear_tabulation/reactor_.cpp
+++ preconditioning_for_linear_tabulation/reactor_.cpp
@@ -276,8 +276,8 @@ void myfgh(int need[], int &nx, double x[], int &nf, int &nh, int iusr[],
     double T[1];                  // temperature
     double ptcl[nx];              // particle properties
     double *solution;             // Cantera solution object
-    double aTol = 1e-8;           // rusr[2*nx];
-    double rTol = 1e-8;           // rusr[2*nx+1]; //absolute and relative tolerances for the ODE integrator
+    double aTol = 1e-3;           // rusr[2*nx];
+    double rTol = 1e-3;           // rusr[2*nx+1]; //absolute and relative tolerances for the ODE integrator
     double dt = rusr[2 * nx + 2]; // time step over which to integrate
     double dx = rusr[2 * nx + 3]; // spatial increment in x for Jacobian evaluation
     double p = rusr[2 * nx + 4];  // user-specified pressure
