39a7d78ea5d92060089d377c489b12591da19c59
jarcities <Hawaiian647@gmail.com>
2025-10-03 10:24:34 -0700

    updated build.sh (ITER)

--- preconditioning_for_linear_tabulation/build.sh
+++ preconditioning_for_linear_tabulation/build.sh
@@ -1,9 +1,16 @@
-export CANTERA_INCLUDE_DIR=/mnt/c/complete/cantera/include
-export CANTERA_LIB_DIR=/mnt/c/complete/cantera/build/lib
-export ISATAB_SERIAL_DIR=/mnt/c/complete/ISAT/isatab_ser
-export ISAT_LIB_DIR=/mnt/c/complete/ISAT/lib
+#!/bin/bash
 rm *.o
 rm *.mod
-rm main.exe
-icpx -O3 -L$CANTERA_LIB_DIR -I$CANTERA_INCLUDE_DIR -c reactor.cpp -lcantera
-ifx -O3 -L$CANTERA_LIB_DIR -I$CANTERA_INCLUDE_DIR -o PaSR.exe PaSR.f90 reactor.o -lstdc++ -lcantera -Bstatic -L$ISAT_LIB_DIR -I$ISATAB_SERIAL_DIR -lisat7_ser -L -mkl -Bdynamic -mkl -lstdc++
+rm *.exe
+
+icpx -O3 -I$CONDA_PREFIX/include -c reactor.cpp 
+
+ifx -O3 \
+    -L$CONDA_PREFIX/lib \
+    -I$CONDA_PREFIX/include \
+    -o PaSR.exe \
+    PaSR.f90 reactor.o \
+    -lstdc++ -lcantera \
+    -Bstatic -L$HOME/code/isat/ISAT/lib -I$HOME/code/isat/ISAT/isatab_ser -lisat7_ser \
+    -qmkl -Bdynamic -qmkl \
+    -Wl,-rpath,$CONDA_PREFIX/lib
